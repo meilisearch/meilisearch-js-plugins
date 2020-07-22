e80f6ce35090ce79d8586d47b40ecc186f61b15d
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-07-22 15:13:55 +0200

    Add linting and styling

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['standard', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {},
+}
