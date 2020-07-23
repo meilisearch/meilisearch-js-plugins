71f88b2756d2c8adbaecb5664a9fb0461a22021a
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-07-23 18:00:11 +0200

    Simple esm test

--- examples/esm/src/index.js
+++ examples/esm/src/index.js
@@ -1,6 +1,7 @@
-import instantMeiliSearch from '../instant-meilisearch.esm'
+// import instantMeiliSearch from '../instant-meilisearch.esm'
 
-console.log(instantMeiliSearch)
-const client = instantMeiliSearch('http://localhost:7700', 'masterKey')
-console.log({ client })
+// console.log(instantMeiliSearch)
+// const client = instantMeiliSearch('http://localhost:7700', 'masterKey')
+// console.log({ client })
 
+console.log('HEY HEY HEY')
