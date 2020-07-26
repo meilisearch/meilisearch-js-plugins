1b63cd93e12fed75563a0b5d4da7e3aab8598d96
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-07-26 18:02:40 +0200

    Better naming for browser tests

--- examples/express/tests/client.test.js
+++ examples/express/tests/client.test.js
@@ -1,9 +1,9 @@
-describe('Google', () => {
+describe('Instant MeiliSearch Browser test', () => {
   beforeAll(async () => {
     await page.goto('http://localhost:3000')
   })
 
-  it('should be titled "Google"', async () => {
+  it('Should have generated a instant-meiisearch client and displayed', async () => {
     await expect(page).toMatch(
       '{"client":{"cancelTokenSource":{"token":{"promise":{}}},"config":{"host":"http://localhost:7700","apiKey":"masterKey"}},"hitsPerPage":10,"limitPerRequest":20,"attributesToHighlight":"*"}'
     )
