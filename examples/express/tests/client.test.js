941683feaca89c0a2ba9fb3dbccaf19f0a2386e6
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-09-22 16:38:07 +0200

    Update front end test

--- examples/express/tests/client.test.js
+++ examples/express/tests/client.test.js
@@ -5,7 +5,7 @@ describe('Instant MeiliSearch Browser test', () => {
 
   it('Should have generated a instant-meiisearch client and displayed', async () => {
     await expect(page).toMatch(
-      'http://localhost:7700'
+      '{"client":{"config":{"host":"http://localhost:7700","apiKey":"masterKey"},"httpRequest":{"headers":{"Content-Type":"application/json","X-Meili-API-Key":"masterKey"},"baseUrl":"http://localhost:7700","url":"http://localhost:7700/"}},"attributesToHighlight":["*"],"paginationTotalHits":200,"placeholderSearch":true}'
     )
   })
 })
