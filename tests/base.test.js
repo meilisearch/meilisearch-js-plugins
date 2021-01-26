e4fbc0a2c80c31915451344c5deaf041771cb7d4
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-01-26 17:40:36 +0100

    Changed to named export instead of default export
    Update playgrounds
    
    Update parcel html
    
    Fix styling

--- tests/base.test.js
+++ tests/base.test.js
@@ -1,4 +1,4 @@
-import instantMeiliSearch from '../src/index'
+import { instantMeiliSearch } from '../src/index'
 
 test('Should test if instantMeiliSearch Client is created correctly', () => {
   const client = instantMeiliSearch('http://localhost:7700', 'masterKey')
