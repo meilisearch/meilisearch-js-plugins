79f3956c821c1cfc1c6b71ec8d3cd05a6cb55bdc
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-02 21:10:30 +0100

    Remove useless ts env

--- tests/env/typescript/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var __1 = require("../../../../");
-var client = __1.instantMeiliSearch('http://localhost:7700', 'masterKey');
-console.log({ client: client });
