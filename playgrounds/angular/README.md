52d1e4735fa040fbbe56a611ab504fe8f8625334
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-09 20:30:36 +0100

    Add angular playground

---
+++ playgrounds/angular/README.md
@@ -0,0 +1,25 @@
+# Angular Playground
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn start
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+## Running end-to-end tests
+
+```
+yarn e2e
+```
+
+Run `yarn e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
---
+++ playgrounds/angular/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+import { instantMeiliSearch } from '../../../../src'
+
+const searchClient = instantMeiliSearch(
+  'https://demos.meilisearch.com',
+  'dc3fedaf922de8937fdea01f0a7d59557f1fd31832cb8440ce94231cfdde7f25'
+)
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'angular-app'
+  config = {
+    indexName: 'steam-video-games',
+    searchClient,
+    // searchFunction: (_) => {},
+  }
+}
