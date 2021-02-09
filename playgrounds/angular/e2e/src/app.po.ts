884d0e29bb5bcb3a1fce2bf188ab74828662ccf0
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-09 21:03:57 +0100

    Fixes linter

--- playgrounds/angular/e2e/src/app.po.ts
+++ playgrounds/angular/e2e/src/app.po.ts
@@ -2,18 +2,18 @@ import { browser, by, element } from 'protractor'
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl)
+    return await browser.get(browser.baseUrl)
   }
 
   async getFirstGame(): Promise<string> {
-    return element(by.css('.ais-Hits-item > .hit-name')).getText()
+    return await element(by.css('.ais-Hits-item > .hit-name')).getText()
   }
 
   async getFirstFacetValueOfFirstFacet(): Promise<string> {
-    return element(by.css('.ais-RefinementList-labelText')).getText()
+    return await element(by.css('.ais-RefinementList-labelText')).getText()
   }
 
   async getClearRefinementText(): Promise<string> {
-    return element(by.css('.ais-ClearRefinements-button')).getText()
+    return await element(by.css('.ais-ClearRefinements-button')).getText()
   }
 }
