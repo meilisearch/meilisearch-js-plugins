797077ebae143f6f46a630247187db92a92ba2e2
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-03-21 18:23:33 +0100

    Update e2e tests

--- tests/env/express/tests/client.test.js
+++ tests/env/express/tests/client.test.js
@@ -5,7 +5,7 @@ describe('Instant MeiliSearch Browser test', () => {
 
   it('Should have generated a instant-meilisearch client and displayed', async () => {
     await expect(page.content()).resolves.toMatch(
-      '{"client":{"config":{"host":"http://localhost:7700/","apiKey":"masterKey"},"httpRequest":{"headers":{"Content-Type":"application/json","X-Meili-API-Key":"masterKey"},"url":"http://localhost:7700/"}},"attributesToHighlight":["*"],"paginationTotalHits":200,"placeholderSearch":true,"hitsPerPage":20}'
+      '{"client":{"config":{"host":"http://localhost:7700/","apiKey":"masterKey"},"httpRequest":{"headers":{"Content-Type":"application/json","X-Meili-API-Key":"masterKey"},"url":"http://localhost:7700/"}},"attributesToHighlight":["*"],"paginationTotalHits":200,"placeholderSearch":true,"hitsPerPage":20,"page":0}'
     )
   })
 })
