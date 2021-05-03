c4fdcae29c08bc6338b786d58e56d298821cc1a0
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-05-03 18:22:28 +0200

    Add pagination tests

---
+++ src/transformers/__tests__/utils.ts
@@ -0,0 +1,14 @@
+import { MeiliSearch } from 'meilisearch'
+const HOST = 'http://localhost:7700'
+// const apiKey = ''
+
+const defaultContext = {
+  client: new MeiliSearch({ host: HOST }),
+  paginationTotalHits: 200,
+  primaryKey: undefined,
+  placeholderSearch: true,
+  hitsPerPage: 20,
+  page: 0,
+}
+
+export { defaultContext }
