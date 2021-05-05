d20edc58018711f5580d79ecf34a5239437e671f
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-05-05 23:53:24 +0200

    Remove unecessary test

--- tests/configure.tests.ts
+++
@@ -1,9 +0,0 @@
-import { instantMeiliSearch } from '../'
-
-describe('Instant MeiliSearch Browser test', () => {
-  it('Should have generated a instant-meilisearch client and displayed', () => {
-    const client = instantMeiliSearch('http://localhost:7700', '')
-    console.log(client)
-    expect(1).toBe(1)
-  })
-})
