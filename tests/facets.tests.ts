4ec543fa9b3d20a2d21c3f8b6a3ea1f849587420
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-05-11 15:52:02 +0200

    Add with query facet tests

--- tests/facets.tests.ts
+++ tests/facets.tests.ts
@@ -18,7 +18,7 @@ describe('Instant MeiliSearch Browser test', () => {
     )
   })
 
-  test('Test one facet on facetsFilters', async () => {
+  test('Test one facet on facetsFilters without a query', async () => {
     const response = await searchClient.search([
       {
         indexName: 'movies',
@@ -33,7 +33,22 @@ describe('Instant MeiliSearch Browser test', () => {
     expect(hits[0].title).toEqual('Star Wars')
   })
 
-  test('Test multiple on facetsFilters', async () => {
+  test('Test one facet on facetsFilters with a query', async () => {
+    const response = await searchClient.search([
+      {
+        indexName: 'movies',
+        params: {
+          query: 'four',
+          facetFilters: ['genres:Crime'],
+        },
+      },
+    ])
+    const hits = response.results[0].hits
+    expect(hits.length).toEqual(2)
+    expect(hits[0].title).toEqual('Four Rooms')
+  })
+
+  test('Test multiple on facetsFilters without a query', async () => {
     const response = await searchClient.search([
       {
         indexName: 'movies',
@@ -48,7 +63,35 @@ describe('Instant MeiliSearch Browser test', () => {
     expect(hits[0].title).toEqual('Ariel')
   })
 
-  test('Test multiple nested on facetsFilters', async () => {
+  test('Test multiple on facetsFilters with a query', async () => {
+    const response = await searchClient.search([
+      {
+        indexName: 'movies',
+        params: {
+          query: 'ar',
+          facetFilters: ['genres:Comedy', 'genres:Crime'],
+        },
+      },
+    ])
+    const hits = response.results[0].hits
+    expect(hits.length).toEqual(2)
+    expect(hits[0].title).toEqual('Ariel')
+  })
+
+  test('Test multiple nested on facetsFilters without a query', async () => {
+    const params = {
+      indexName: 'movies',
+      params: {
+        query: 'night',
+        facetFilters: [['genres:action', 'genres:Thriller'], 'genres:crime'],
+      },
+    }
+    const response = await searchClient.search([params])
+    const hits = response.results[0].hits
+    expect(hits[0].title).toEqual('Judgment Night')
+  })
+
+  test('Test multiple nested on facetsFilters with a query', async () => {
     const params = {
       indexName: 'movies',
       params: {
