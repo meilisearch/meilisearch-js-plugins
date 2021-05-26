30c02c0214d5140f65822316abacc48fe3e917c4
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-05-26 18:42:52 +0200

    Start component compatibility

--- playgrounds/javascript/src/app.js
+++ playgrounds/javascript/src/app.js
@@ -1,6 +1,8 @@
 import { instantMeiliSearch } from '../../../src/index'
 
+const sessionStorageCache = instantsearch.createInfiniteHitsSessionStorageCache()
 const search = instantsearch({
+  routing: true,
   indexName: 'steam-video-games',
   searchClient: instantMeiliSearch(
     'https://demos.meilisearch.com',
@@ -9,11 +11,37 @@ const search = instantsearch({
       limitPerRequest: 30,
     }
   ),
+  // Used for Ranged
+  // searchFunction(helper) {
+  //   console.log(helper)
+  //   const page = helper.getPage() // Retrieve the current page
+
+  //   helper
+  //     .setQuery('Hello') // this call resets the page
+  //     .setPage(page) // we re-apply the previous page
+  //     .search()
+  // },
+  // initialUiState: {
+  //   'steam-video-games': {
+  //     query: 'phone',
+  //     page: 5,
+  //   },
+  // },
+  // onStateChange({ uiState, setUiState }) {
+  //   console.log({ uiState, setUiState });
+  //   // Custom logic
+
+  //   setUiState(uiState);
+  // },
+  // stalledSearchDelay: 2000,
+  // routing: true,
 })
 
 search.addWidgets([
   instantsearch.widgets.searchBox({
     container: '#searchbox',
+    showLoadingIndicator: false,
+
   }),
   instantsearch.widgets.clearRefinements({
     container: '#clear-refinements',
@@ -39,11 +67,12 @@ search.addWidgets([
   }),
   instantsearch.widgets.hits({
     container: '#hits',
+    showPrevious: true,
     templates: {
       item: `
         <div>
           <div class="hit-name">
-            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+            {{#helpers.highlight}}{ "attribute": "name", "highlightedTagName": "p" }{{/helpers.highlight}}
           </div>
           <img src="{{image}}" align="left" />
           <div class="hit-info">price: {{price}}</div>
@@ -52,9 +81,9 @@ search.addWidgets([
       `,
     },
   }),
-  instantsearch.widgets.pagination({
-    container: '#pagination',
-  }),
+  // instantsearch.widgets.pagination({
+  //   container: '#pagination',
+  // }),
 ])
 
 search.start()
