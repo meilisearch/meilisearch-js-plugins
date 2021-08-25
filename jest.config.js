0d9a2b728dbf02a49313e6a83d30ff01e02697c2
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-08-25 18:05:07 +0200

    Add tests to ensure builds works correctly in node env

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const ignoreFiles = ['<rootDir>/playgrounds', '<rootDir>/tests/assets']
+
 module.exports = {
   verbose: true,
   watchPlugins: [
@@ -6,6 +8,12 @@ module.exports = {
   ],
   projects: [
     {
+      displayName: 'build',
+      testEnvironment: 'node',
+      testPathIgnorePatterns: [...ignoreFiles, '<rootDir>/tests/*.ts'],
+      testMatch: ['**/*.tests.js', '/tests/**/*.js'],
+    },
+    {
       globals: {
         'ts-jest': {
           tsconfig: 'tsconfig.test.json',
@@ -13,14 +21,7 @@ module.exports = {
       },
       preset: 'ts-jest',
       displayName: 'dom',
-      // We are using jest-environment-jsdom 25 until we stop supporting node 10
-      // jest-environment-jsdom 25 uses jsdom 15 which still supports node 10
-      testEnvironment: 'jest-environment-jsdom',
-      testPathIgnorePatterns: [
-        '<rootDir>/tests/env',
-        '<rootDir>/playgrounds',
-        '<rootDir>/tests/assets',
-      ],
+      testPathIgnorePatterns: [...ignoreFiles, '<rootDir>/tests/build*'],
       testMatch: ['**/*.tests.ts', '/tests/**/*.ts'],
     },
     {
@@ -32,11 +33,7 @@ module.exports = {
       preset: 'ts-jest',
       displayName: 'node',
       testEnvironment: 'node',
-      testPathIgnorePatterns: [
-        '<rootDir>/tests/env',
-        '<rootDir>/playgrounds',
-        '<rootDir>/tests/assets',
-      ],
+      testPathIgnorePatterns: [...ignoreFiles],
       testMatch: ['**/*.tests.ts', '/tests/**/*.ts'],
     },
   ],
