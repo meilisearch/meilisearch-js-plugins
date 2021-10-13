a97bdc9c17241f5101716d13bc82be0d0f9d1e6c
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-10-13 12:57:36 +0200

    Rollback app.js to initial main state

--- playgrounds/javascript/src/app.js
+++ playgrounds/javascript/src/app.js
@@ -1,79 +1,79 @@
-import { instantMeiliSearch } from '../../../src/client/index'
-import injectScript from 'scriptjs'
-// console.log(process.env.local)
-console.log(process.env.GOOGLE_API)
+import { instantMeiliSearch } from '../../../src/index'
 
-const GOOGLE_API = process.env.GOOGLE_API
+const search = instantsearch({
+  indexName: 'steam-video-games',
+  searchClient: instantMeiliSearch(
+    'https://demo-steam.meilisearch.com',
+    '90b03f9c47d0f321afae5ae4c4e4f184f53372a2953ab77bca679ff447ecc15c',
+    {
+      limitPerRequest: 30,
+    }
+  ),
+})
 
-injectScript(
-  `https://maps.googleapis.com/maps/api/js?v=quarterly&key=${GOOGLE_API}`,
-  () => {
-    const search = instantsearch({
-      indexName: 'geo',
-      searchClient: instantMeiliSearch('http://localhost:7700', 'masterKey', {
-        limitPerRequest: 30,
-      }),
-      // routing: true,
-      searchFunction: function (helper) {
-        console.log(helper)
-        // helper.setQueryParameter('aroundRadius', 75000)
-        // helper.setQueryParameter('aroundLatLngViaIP', true)
-        // helper.setQueryParameter('query', 'Lille')
-
-        // helper.setQueryParameter('aroundRadius', somevalue);
-        helper.search()
+search.addWidgets([
+  instantsearch.widgets.sortBy({
+    container: '#sort-by',
+    items: [
+      { value: 'steam-video-games', label: 'Relevant' },
+      {
+        value: 'steam-video-games:recommendationCount:desc',
+        label: 'Most Recommended',
       },
-      // urlSync: {
-      //   trackedParameters: [
-      //     'attribute:*',
-      //     'query',
-      //     'page',
-      //     'hitsPerPage',
-      //     'aroundLatLngViaIP',
-      //     'aroundRadius',
-      //     'insideBoundingBox',
-      //   ],
-      // },
-    })
+      {
+        value: 'steam-video-games:recommendationCount:asc',
+        label: 'Least Recommended',
+      },
+    ],
+  }),
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#genres-list',
+    attribute: 'genres',
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#players-list',
+    attribute: 'players',
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#platforms-list',
+    attribute: 'platforms',
+  }),
+  instantsearch.widgets.configure({
+    hitsPerPage: 6,
+    attributesToSnippet: ['description:150'],
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#misc-list',
+    attribute: 'misc',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <div>
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+          </div>
+          <img src="{{image}}" align="left" />
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-info">price: {{price}}</div>
+          <div class="hit-info">release date: {{releaseDate}}</div>
+          <div class="hit-info">Recommendation: {{recommendationCount}}</div>
+        </div>
+      `,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+])
 
-    search.addWidgets([
-      instantsearch.widgets.searchBox({
-        container: '#searchbox',
-      }),
-      instantsearch.widgets.clearRefinements({
-        container: '#clear-refinements',
-      }),
-      instantsearch.widgets.configure({
-        hitsPerPage: 6,
-        attributesToSnippet: ['description:150'],
-      }),
-      instantsearch.widgets.geoSearch({
-        container: '#maps',
-        googleReference: window.google,
-        initialZoom: 5,
-        initialPosition: {
-          lat: 50.655250871381355,
-          lng: 4.843585698860502,
-        },
-      }),
-      instantsearch.widgets.hits({
-        container: '#hits',
-        templates: {
-          item: `
-            <div>
-              <div class="hit-name">
-                {{#helpers.highlight}}{ "attribute": "city" }{{/helpers.highlight}}
-              </div>
-              <div class="hit-info">id: {{id}}</div>
-            </div>
-          `,
-        },
-      }),
-      instantsearch.widgets.pagination({
-        container: '#pagination',
-      }),
-    ])
-
-    search.start()
-  }
-)
+search.start()
