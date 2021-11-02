88147691e54cf697786396d470211c1c1a9a9c93
CaroFG <carolina.ferreira131@gmail.com>
2021-11-02 14:56:01 +0100

    Replace "countries" with "world_cities"

--- playgrounds/geo-javascript/setup/index.js
+++ playgrounds/geo-javascript/setup/index.js
@@ -1,8 +1,8 @@
 const settings = require('./settings.json')
-const countries = require('./countries.json')
+const cities = require('./world-cities.json')
 const { MeiliSearch } = require('meilisearch')
 
-const INDEX_UID = 'countries_playground'
+const INDEX_UID = 'cities_playground'
 ;(async () => {
   const client = new MeiliSearch({
     host: 'http://localhost:7700',
@@ -11,7 +11,7 @@ const INDEX_UID = 'countries_playground'
   try {
     await client.deleteIndex(INDEX_UID)
   } catch (e) {
-    // countries_playground does not exist
+    // cities_playground does not exist
   }
   const settingsUpdate = await client.index(INDEX_UID).updateSettings(settings)
   const status = await client
@@ -25,8 +25,8 @@ const INDEX_UID = 'countries_playground'
       status
     )
   }
-  const countriesUpdate = await client.index(INDEX_UID).addDocuments(countries)
-  await client.index(INDEX_UID).waitForPendingUpdate(countriesUpdate.updateId)
+  const citiesUpdate = await client.index(INDEX_UID).addDocuments(cities)
+  await client.index(INDEX_UID).waitForPendingUpdate(citiesUpdate.updateId)
   const documents = await client.index(INDEX_UID).getDocuments()
   if (documents.length === 20) {
     console.log('SUCCESS: All 20 documents have been succesfully indexed.')
