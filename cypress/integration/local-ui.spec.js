6ae16d741ef689aa2966c968172c3a3b6beb760f
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2022-01-06 17:17:47 +0100

    Add pre release process (#632)
    * Update README.md
    
    * Change index creation and wait method in tests
    
    * Add types tests
    
    * Change index creation and wait method in tests (#629)
    
    * Add types tests (#630)
    
    * Change index creation and wait method in tests
    
    * Add types tests
    
    * Update meilisearch js
    
    * Create local e2e tests
    
    * Add pre release tests
    
    * Add pre-release publis
    
    * Add Beta publish in contributing
    
    * Add docker in CI
    
    * Change way to launch meilisearch in CI tests
    
    * Update script to fetch latest meilisearch
    
    * Remove vue from types tests
    
    * Only launch local tests on pre-release
    
    * Update playgrounds/angular/src/app/app.component.ts
    
    * Rollback meilisearch js version
    
    Co-authored-by: meili-bot <74670311+meili-bot@users.noreply.github.com>

---
+++ cypress/integration/local-ui.spec.js
@@ -0,0 +1,85 @@
+const {
+  playground,
+  [playground]: { host },
+} = Cypress.env()
+
+const HIT_ITEM_CLASS =
+  playground === 'react' || playground === 'reactlocal'
+    ? '.ais-InfiniteHits-item'
+    : '.ais-Hits-item'
+
+describe(`${playground} playground test`, () => {
+  before(() => {
+    cy.clearCookies()
+    cy.visit(host)
+  })
+
+  it('Should visit the dashboard', () => {
+    cy.url().should('match', /http:\/\/localhost:/)
+  })
+
+  it('Contains stats', () => {
+    cy.contains('15 results')
+  })
+
+  it('Contains filter clear', () => {
+    cy.contains('Clear all filters')
+  })
+
+  it('Contains Genres', () => {
+    cy.contains('Action')
+  })
+
+  it('Contains searchBar', () => {
+    cy.get('.ais-SearchBox-input').should('have.value', '')
+  })
+
+  it('Contains Hits', () => {
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('Counter-Strike')
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('9.99 $')
+  })
+
+  it('Sort by recommendationCound ascending', () => {
+    const select = `.ais-SortBy-select`
+    cy.get(select).select('steam-video-games:recommendationCount:asc')
+    cy.wait(1000)
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('Deathmatch Classic')
+  })
+
+  it('Sort by default relevancy', () => {
+    const select = `.ais-SortBy-select`
+    cy.get(select).select('steam-video-games')
+    cy.wait(1000)
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('Counter-Strike')
+  })
+
+  it('click on facets', () => {
+    const checkbox = `.ais-RefinementList-list .ais-RefinementList-checkbox`
+    cy.get(checkbox).eq(1).click()
+    cy.get(HIT_ITEM_CLASS).eq(1).contains('Team Fortress Classic')
+    cy.get(HIT_ITEM_CLASS).eq(1).contains('4.99 $')
+  })
+
+  it('Search', () => {
+    cy.get('.ais-SearchBox-input').type('Half-Life')
+    cy.wait(1000)
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('Half-Life')
+  })
+
+  it('Unclick on facets', () => {
+    const checkbox = `.ais-RefinementList-list .ais-RefinementList-checkbox`
+    cy.get(checkbox).eq(0).click()
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('Half-Life')
+  })
+
+  it('Placeholder Search', () => {
+    cy.get('.ais-SearchBox-input').clear()
+    cy.wait(1000)
+    cy.get(HIT_ITEM_CLASS).eq(0).contains('Counter-Strike')
+  })
+
+  it('Paginate Search', () => {
+    cy.get('.ais-InfiniteHits-loadMore').click()
+    cy.get(HIT_ITEM_CLASS).should('have.length', 11)
+  })
+})
