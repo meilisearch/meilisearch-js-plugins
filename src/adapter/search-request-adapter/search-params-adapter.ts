fe11182548f071d855cf3233663c5fd0bc76b9bf
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-04-04 18:35:12 +0200

    Add builder for the search params

--- src/adapter/search-request-adapter/search-params-adapter.ts
+++ src/adapter/search-request-adapter/search-params-adapter.ts
@@ -6,6 +6,92 @@ import {
 } from './geo-rules-adapter'
 import { adaptFilters } from './filter-adapter'
 
+function ParamsBuilder(searchContext: SearchContext) {
+  const meiliSearchParams: Record<string, any> = {}
+
+  return {
+    getParams() {
+      return meiliSearchParams
+    },
+    addFacets() {
+      const facets = searchContext?.facets
+      if (facets?.length) {
+        meiliSearchParams.facetsDistribution = facets
+      }
+    },
+    addAttributesToCrop() {
+      const attributesToCrop = searchContext?.attributesToSnippet
+      if (attributesToCrop) {
+        meiliSearchParams.attributesToCrop = attributesToCrop
+      }
+    },
+    addAttributesToRetrieve() {
+      const attributesToRetrieve = searchContext?.attributesToRetrieve
+      if (attributesToRetrieve) {
+        meiliSearchParams.attributesToRetrieve = attributesToRetrieve
+      }
+    },
+    addFilters() {
+      const filter = adaptFilters(
+        searchContext?.filters,
+        searchContext?.numericFilters,
+        searchContext?.facetFilters
+      )
+      if (filter.length) {
+        meiliSearchParams.filter = filter
+      }
+    },
+    addAttributesToHighlight() {
+      meiliSearchParams.attributesToHighlight = searchContext?.attributesToHighlight || [
+        '*',
+      ]
+    },
+    addLimit() {
+      const placeholderSearch = searchContext.placeholderSearch
+      const query = searchContext.query
+      const { pagination } = searchContext
+
+      // Limit based on pagination preferences
+      if (
+        (!placeholderSearch && query === '') ||
+        pagination.paginationTotalHits === 0
+      ) {
+        meiliSearchParams.limit = 0
+      } else if (searchContext.finitePagination) {
+        meiliSearchParams.limit = pagination.paginationTotalHits
+      } else {
+        const limit = (pagination.page + 1) * pagination.hitsPerPage + 1
+        // If the limit is bigger than the total hits accepted
+        // force the limit to that amount
+        if (limit > pagination.paginationTotalHits) {
+          meiliSearchParams.limit = pagination.paginationTotalHits
+        } else {
+          meiliSearchParams.limit = limit
+        }
+      }
+    },
+    addSort() {
+      const sort = searchContext.sort
+
+      if (sort?.length) {
+        meiliSearchParams.sort = [sort]
+      }
+    },
+    addGeoSearchRules() {
+      const geoSearchContext = createGeoSearchContext(searchContext)
+      const geoRules = adaptGeoPointsRules(geoSearchContext)
+
+      if (geoRules?.filter) {
+        if (meiliSearchParams.filter) {
+          meiliSearchParams.filter.unshift(geoRules.filter)
+        } else {
+          meiliSearchParams.filter = [geoRules.filter]
+        }
+      }
+    },
+  }
+}
+
 /**
  * Adapt search request from instantsearch.js
  * to search request compliant with Meilisearch
@@ -16,89 +102,15 @@ import { adaptFilters } from './filter-adapter'
 export function adaptSearchParams(
   searchContext: SearchContext
 ): MeiliSearchParams {
-  // Creates search params object compliant with Meilisearch
-  const meiliSearchParams: Record<string, any> = {}
+  const buildParams = ParamsBuilder(searchContext)
+  buildParams.addFacets()
+  buildParams.addFilters()
+  buildParams.addLimit()
+  buildParams.addSort()
+  buildParams.addGeoSearchRules()
+  buildParams.addAttributesToCrop()
+  buildParams.addAttributesToHighlight()
+  buildParams.addAttributesToRetrieve()
 
-  // Facets
-  const facets = searchContext?.facets
-  if (facets?.length) {
-    meiliSearchParams.facetsDistribution = facets
-  }
-
-  // Attributes To Crop
-  const attributesToCrop = searchContext?.attributesToSnippet
-  if (attributesToCrop) {
-    meiliSearchParams.attributesToCrop = attributesToCrop
-  }
-
-  // Attributes To Retrieve
-  const attributesToRetrieve = searchContext?.attributesToRetrieve
-  if (attributesToRetrieve) {
-    meiliSearchParams.attributesToRetrieve = attributesToRetrieve
-  }
-
-  // Filter
-  const filter = adaptFilters(
-    searchContext?.filters,
-    searchContext?.numericFilters,
-    searchContext?.facetFilters
-  )
-  if (filter.length) {
-    meiliSearchParams.filter = filter
-  }
-
-  // Attributes To Retrieve
-  if (attributesToRetrieve) {
-    meiliSearchParams.attributesToCrop = attributesToRetrieve
-  }
-
-  // Attributes To Highlight
-  meiliSearchParams.attributesToHighlight = searchContext?.attributesToHighlight || [
-    '*',
-  ]
-
-  const placeholderSearch = searchContext.placeholderSearch
-  const query = searchContext.query
-
-  // Pagination
-  const { pagination } = searchContext
-
-  // Limit based on pagination preferences
-  if (
-    (!placeholderSearch && query === '') ||
-    pagination.paginationTotalHits === 0
-  ) {
-    meiliSearchParams.limit = 0
-  } else if (searchContext.finitePagination) {
-    meiliSearchParams.limit = pagination.paginationTotalHits
-  } else {
-    const limit = (pagination.page + 1) * pagination.hitsPerPage + 1
-    // If the limit is bigger than the total hits accepted
-    // force the limit to that amount
-    if (limit > pagination.paginationTotalHits) {
-      meiliSearchParams.limit = pagination.paginationTotalHits
-    } else {
-      meiliSearchParams.limit = limit
-    }
-  }
-
-  const sort = searchContext.sort
-
-  // Sort
-  if (sort?.length) {
-    meiliSearchParams.sort = [sort]
-  }
-
-  const geoSearchContext = createGeoSearchContext(searchContext)
-  const geoRules = adaptGeoPointsRules(geoSearchContext)
-
-  if (geoRules?.filter) {
-    if (meiliSearchParams.filter) {
-      meiliSearchParams.filter.unshift(geoRules.filter)
-    } else {
-      meiliSearchParams.filter = [geoRules.filter]
-    }
-  }
-
-  return meiliSearchParams
+  return buildParams.getParams()
 }
