71f197d7be172d41b686637aeb70e1b2b77f1e45
Charlotte Vermandel <charlottevermandel@charlottes-macbook-pro.home>
2022-06-30 11:06:48 +0200

    Improve check release script

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -2,17 +2,16 @@
 
 # Checking if current tag matches the package version
 current_tag=$(echo $GITHUB_REF | cut -d '/' -f 3 | tr -d ' ',v)
-file_tag=$(grep '"version":' package.json | cut -d ':' -f 2- | tr -d ' ' | tr -d '"' | tr -d ',')
-if [ "$current_tag" != "$file_tag" ]; then
-  echo "Error: the current tag does not match the version in package file(s)."
-  echo "$current_tag vs $file_tag"
-  exit 1
-fi
 
-package_version_ts=$(grep "PACKAGE_VERSION =" src/package-version.ts | cut -d "=" -f 2- | tr -d " " | tr -d "'")
-if [ "$current_tag" != "$package_version_ts" ]; then
-  echo "Error: the current tag does not match the version in src/package-version.ts."
-  echo "$current_tag vs $package_version_ts"
+package_file_tag=$(grep '"version":' package.json | cut -d ':' -f 2- | tr -d ' ' | tr -d '"' | tr -d ',')
+package_file_name="package.json"
+version_file_tag=$(grep "PACKAGE_VERSION =" src/package-version.ts | cut -d "=" -f 2- | tr -d " " | tr -d "'")
+version_file_name="src/package-version.ts"
+
+if [ "$current_tag" != "$package_file_tag" ] || [ "$current_tag" != "$version_file_tag"]; then
+  echo "Error: the current tag does not match the version in package file(s)."
+  echo "$package_file_name: $current_tag vs $package_file_tag"
+  echo "$version_file_name: $current_tag vs $version_file_tag"
   exit 1
 fi
 
