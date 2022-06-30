903c42244c0ee1157d7ccab85ba53437602a8e4d
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2022-06-30 15:54:04 +0200

    Pass client agent to meilisearch-js (#780)
    * Update search route request and response parameters naming v0.28.x
    
    * Pass client agent to meilisearch-js
    
    * Improve check release script
    
    * Use double quotes on string with no dynamic variable in check releases script
    
    * Add missing whitespace in check-release.sh
    
    * Add possibility to provide client agents through the parameters of instantMeilisearch
    
    * Fix search resolver tests
    
    Co-authored-by: Charlotte Vermandel <charlottevermandel@charlottes-macbook-pro.home>

--- src/client/instant-meilisearch-client.ts
+++ src/client/instant-meilisearch-client.ts
@@ -13,6 +13,7 @@ import {
 } from '../adapter'
 import { createSearchContext } from '../contexts'
 import { SearchCache, cacheFirstFacetDistribution } from '../cache/'
+import { constructClientAgents } from './agents'
 
 /**
  * Instanciate SearchClient required by instantsearch.js.
@@ -31,7 +32,15 @@ export function instantMeiliSearch(
   const searchResolver = SearchResolver(SearchCache())
   // paginationTotalHits can be 0 as it is a valid number
   let defaultFacetDistribution: any = {}
-  const meilisearchClient = new MeiliSearch({ host: hostUrl, apiKey: apiKey })
+  const clientAgents = constructClientAgents(
+    instantMeiliSearchOptions.clientAgents
+  )
+
+  const meilisearchClient = new MeiliSearch({
+    host: hostUrl,
+    apiKey: apiKey,
+    clientAgents,
+  })
 
   return {
     /**
