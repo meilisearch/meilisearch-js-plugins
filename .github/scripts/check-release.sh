f98d778729dd822531d3db9170d526fe034b6c94
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-06-30 13:15:55 +0200

    Use double quotes on string with no dynamic variable in check releases script

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -4,12 +4,12 @@
 current_tag=$(echo $GITHUB_REF | cut -d '/' -f 3 | tr -d ' ',v)
 
 package_file_tag=$(grep '"version":' package.json | cut -d ':' -f 2- | tr -d ' ' | tr -d '"' | tr -d ',')
-package_file_name="package.json"
+package_file_name='package.json'
 version_file_tag=$(grep "PACKAGE_VERSION =" src/package-version.ts | cut -d "=" -f 2- | tr -d " " | tr -d "'")
-version_file_name="src/package-version.ts"
+version_file_name='src/package-version.ts'
 
 if [ "$current_tag" != "$package_file_tag" ] || [ "$current_tag" != "$version_file_tag"]; then
-  echo "Error: the current tag does not match the version in package file(s)."
+  echo 'Error: the current tag does not match the version in package file(s).'
   echo "$package_file_name: $current_tag vs $package_file_tag"
   echo "$version_file_name: $current_tag vs $version_file_tag"
   exit 1
