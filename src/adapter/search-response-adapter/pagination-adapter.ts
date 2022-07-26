8d6c4f4ca06d94c0fd8c3bd98f232e5b0b90e359
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-07-26 12:49:18 +0200

    Remove unecessary function

--- src/adapter/search-response-adapter/pagination-adapter.ts
+++ src/adapter/search-response-adapter/pagination-adapter.ts
@@ -1,29 +1,6 @@
 import type { MeiliSearchResponse, PaginationContext } from '../../types'
 import { ceiledDivision } from '../../utils'
 
-/**
- * Slice the requested hits based on the pagination position.
- *
- * @param  {Record<string} hits
- * @param  {number} page
- * @param  {number} hitsPerPage
- * @returns {Array}
- */
-export function adaptPagination(
-  hits: Record<string, any>,
-  page: number,
-  hitsPerPage: number
-): Array<Record<string, any>> {
-  if (hitsPerPage < 0) {
-    throw new TypeError(
-      'Value too small for "hitsPerPage" parameter, expected integer between 0 and 9223372036854775807'
-    )
-  }
-  const start = page * hitsPerPage
-  return hits.slice(start, start + hitsPerPage)
-}
-
-// export function adaptNbPages(): number {}
 function adaptHitsPerPage(
   searchResponse: MeiliSearchResponse<Record<string, any>>,
   paginationContext: PaginationContext
