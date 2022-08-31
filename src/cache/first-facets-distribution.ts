f986d17dddd2d23f01aa319c171f324ec6bac697
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-08-31 11:44:11 +0200

    Ensure default search request is made with a limit of 0

--- src/cache/first-facets-distribution.ts
+++ src/cache/first-facets-distribution.ts
@@ -5,15 +5,23 @@ export async function cacheFirstFacetDistribution(
   searchResolver: any,
   searchContext: SearchContext
 ): Promise<FacetDistribution> {
-  const meilisearchParams = MeiliParamsCreator(searchContext)
+  const defaultSearchContext = {
+    ...searchContext,
+    // placeholdersearch true to ensure a request is made
+    placeholderSearch: true,
+    // Set paginationTotalHits to ensure limit is set to 0
+    // in order to retrieve 0 documents during the default search request
+    pagination: { ...searchContext.pagination, paginationTotalHits: 0 },
+    // query set to empty to ensure retrieving the default facetdistribution
+    query: '',
+  }
+  const meilisearchParams = MeiliParamsCreator(defaultSearchContext)
   meilisearchParams.addFacets()
-  meilisearchParams.addAttributesToRetrieve()
+  meilisearchParams.addPagination()
 
   // Search response from Meilisearch
   const searchResponse = await searchResolver.searchResponse(
-    // placeholdersearch true to ensure a request is made
-    // query set to empty to ensure default facetdistributionap
-    { ...searchContext, placeholderSearch: true, query: '' },
+    defaultSearchContext,
     meilisearchParams.getParams()
   )
   return searchResponse.facetDistribution || {}
