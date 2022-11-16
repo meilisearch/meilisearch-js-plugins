f62a29744e63f03b9d13a5a44f101ca939b53cf0
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-11-16 11:44:35 +0100

    Improve floor division

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -14,12 +14,11 @@ export function ceiledDivision(dividend: number, divisor: number): number {
 /**
  * @param  {number} dividend
  * @param  {number} divisor
- * @returns number
+ * @returns {number}
  */
 export function floorDivision(dividend: number, divisor: number): number {
   if (divisor > 0) {
-    const NumberPages = Math.floor(dividend / divisor) // total number of pages rounded up to the next largest integer.
-    return NumberPages
+    return Math.floor(dividend / divisor)
   }
   return 0
 }
