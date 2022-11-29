4547b56436abe7c819c4548ede2727d7ccf9f433
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-11-29 15:09:37 +0100

    remove console log

--- scripts/jest_teardown.js
+++ scripts/jest_teardown.js
@@ -4,8 +4,6 @@ const HOST = 'http://localhost:7700'
 const API_KEY = 'masterKey'
 
 afterAll(async () => {
-  console.log('DONE')
-
   const client = new MeiliSearch({ host: HOST, apiKey: API_KEY })
   await client.deleteIndex('movies')
   const task = await client.deleteIndex('games')
