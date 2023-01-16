168fefe1965667b28ee6906e1c366caca8f5565f
Charlotte Vermandel <charlottevermandel@gmail.com>
2023-01-16 16:34:34 +0100

    Enable linting on @meilisearch/instant-meilisearch

--- packages/eslint-config-meilisearch/index.js
+++ packages/eslint-config-meilisearch/index.js
@@ -132,7 +132,7 @@ module.exports = {
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        project: ['tsconfig.json'],
+        project: ['tsconfig.eslint.json'],
         projectFolderIgnoreList: ['dist'],
       },
 
