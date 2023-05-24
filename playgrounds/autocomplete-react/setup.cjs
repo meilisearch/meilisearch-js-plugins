24d00d0d70086cb2b4060e2ba29b5101a6cee8db
Charlotte Vermandel <charlottevermandel@gmail.com>
2023-05-24 17:52:50 +0200

    Add cypress tests

--- playgrounds/autocomplete-react/setup.cjs
+++ playgrounds/autocomplete-react/setup.cjs
@@ -1,11 +1,12 @@
 const { MeiliSearch } = require('meilisearch')
-const products = require('./bestbuy.json')
+const products = require('./assets/games.json')
 
 ;(async () => {
   const client = new MeiliSearch({
     host: 'http://localhost:7700',
     apiKey: 'masterKey',
   })
+  await client.index('products').delete()
   const task = await client.index('products').addDocuments(products)
   await client.waitForTask(task.taskUid)
 })()
--- playgrounds/autocomplete-react/src/app.js
+++ playgrounds/autocomplete-react/src/app.js
@@ -3,7 +3,6 @@ import {
   meilisearchAutocompleteClient,
   getMeilisearchResults,
 } from '@meilisearch/autocomplete-client'
-
 import '@algolia/autocomplete-theme-classic'
 
 const client = meilisearchAutocompleteClient(
@@ -16,7 +15,6 @@ autocomplete({
   placeholder: 'Search for products',
   openOnFocus: true,
   debug: true,
-  // plugins: [querySuggestionsPlugin],
   getSources({ query }) {
     return [
       {
@@ -30,14 +28,17 @@ autocomplete({
                 query,
                 params: {
                   hitsPerPage: 10,
-                  attributesToSnippet: ['name:10', 'description:35'],
-                  snippetEllipsisText: '…',
+                  attributesToSnippet: ['name:10', 'description:5'],
+                  snippetEllipsisText: '..',
                 },
               },
             ],
           })
 
           return description
+        },
+        getItemUrl({ item }) {
+          return item.image
         },
         templates: {
           item({ item, components, html }) {
@@ -56,6 +57,7 @@ autocomplete({
                     ${components.Highlight({
                       hit: item,
                       attribute: 'name',
+                      tagName: 'test',
                     })}
                   </div>
                   <div class="aa-ItemContentDescription">
@@ -79,22 +81,6 @@ autocomplete({
                     >
                       <path
                         d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z"
-                      />
-                    </svg>
-                  </button>
-                  <button
-                    class="aa-ItemActionButton"
-                    type="button"
-                    title="Add to cart"
-                  >
-                    <svg
-                      viewBox="0 0 24 24"
-                      width="18"
-                      height="18"
-                      fill="currentColor"
-                    >
-                      <path
-                        d="M19 5h-14l1.5-2h11zM21.794 5.392l-2.994-3.992c-0.196-0.261-0.494-0.399-0.8-0.4h-12c-0.326 0-0.616 0.156-0.8 0.4l-2.994 3.992c-0.043 0.056-0.081 0.117-0.111 0.182-0.065 0.137-0.096 0.283-0.095 0.426v14c0 0.828 0.337 1.58 0.879 2.121s1.293 0.879 2.121 0.879h14c0.828 0 1.58-0.337 2.121-0.879s0.879-1.293 0.879-2.121v-14c0-0.219-0.071-0.422-0.189-0.585-0.004-0.005-0.007-0.010-0.011-0.015zM4 7h16v13c0 0.276-0.111 0.525-0.293 0.707s-0.431 0.293-0.707 0.293h-14c-0.276 0-0.525-0.111-0.707-0.293s-0.293-0.431-0.293-0.707zM15 10c0 0.829-0.335 1.577-0.879 2.121s-1.292 0.879-2.121 0.879-1.577-0.335-2.121-0.879-0.879-1.292-0.879-2.121c0-0.552-0.448-1-1-1s-1 0.448-1 1c0 1.38 0.561 2.632 1.464 3.536s2.156 1.464 3.536 1.464 2.632-0.561 3.536-1.464 1.464-2.156 1.464-3.536c0-0.552-0.448-1-1-1s-1 0.448-1 1z"
                       />
                     </svg>
                   </button>
