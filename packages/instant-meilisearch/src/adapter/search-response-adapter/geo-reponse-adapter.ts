e15136b214ec700058903e1761e1a87ae1424742
amy null <null@amy.gg>
2024-08-03 23:31:09 -0400

    🐛 fix(geosearch): Don't overwrite object ID in geosearch response

--- packages/instant-meilisearch/src/adapter/search-response-adapter/geo-reponse-adapter.ts
+++ packages/instant-meilisearch/src/adapter/search-response-adapter/geo-reponse-adapter.ts
@@ -7,12 +7,16 @@ export function adaptGeoResponse(hits: any[]): Array<Record<string, any>> {
     const objectID = `${i + Math.random() * 1000000}`
     if (hits[i]._geo) {
       hits[i]._geoloc = hits[i]._geo
-      hits[i].objectID = objectID
+      if (!hits[i].objectID) {
+        hits[i].objectID = objectID
+      }
     }
 
     if (hits[i]._formatted?._geo) {
       hits[i]._formatted._geoloc = hits[i]._formatted._geo
-      hits[i]._formatted.objectID = objectID
+      if (!hits[i]._formatted.objectID) {
+        hits[i]._formatted.objectID = objectID
+      }
     }
   }
   return hits
