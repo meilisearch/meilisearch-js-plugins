16b5b47327665c7db75eb89fcff5512d174a9b05
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-10-07 10:38:00 +0300

    Revert "Make error more detailed temporarily"
    This reverts commit d0ae474755182ec254209b3190b7a6cc4170c924.

--- packages/instant-meilisearch/scripts/jest_teardown.js
+++ packages/instant-meilisearch/scripts/jest_teardown.js
@@ -1,24 +1,12 @@
-const { MeiliSearch } = require('meilisearch');
+const { MeiliSearch } = require('meilisearch')
 
-const HOST = 'http://localhost:7700';
-const API_KEY = 'masterKey';
+const HOST = 'http://localhost:7700'
+const API_KEY = 'masterKey'
 
 afterAll(async () => {
-  try {
-    const client = new MeiliSearch({ host: HOST, apiKey: API_KEY });
-    await client.deleteIndex('movies');
-    const task = await client.deleteIndex('games');
+  const client = new MeiliSearch({ host: HOST, apiKey: API_KEY })
+  await client.deleteIndex('movies')
+  const task = await client.deleteIndex('games')
 
-    await client.waitForTask(task.taskUid);
-  } catch (error) {
-    if ('cause' in error) {
-      if ('message' in error.cause) {
-        error.message += error.cause.message;
-      } else {
-        error.message += error.cause;
-      }
-    }
-
-    throw error;
-  }
-});
+  await client.waitForTask(task.taskUid)
+})
