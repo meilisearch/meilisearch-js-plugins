cc2ba747b08ef3e898b41aeff40e620dd861d700
Strift <lau.cazanove@gmail.com>
2025-01-15 19:46:14 +0800

    Fix typescript build

---
+++ packages/instant-meilisearch/src/types/dep.d.ts
@@ -0,0 +1,7 @@
+declare module 'instantsearch.js' {
+  export { SearchClient } from 'instantsearch.js/es/types'
+}
+// 	export { InstantSearchSSRProvider } from '/#react-instantsearch-hooks/dist/es/index.js'
+// 	export { Pagination } from '/#react-instantsearch-hooks/dist/es/index.js'
+// 	export { SearchBox } from '/#react-instantsearch-hooks/dist/es/index.js'
+// }
