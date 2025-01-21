28793693e08d577c1247b20074decc70b623a07b
F. Levi <55688616+flevi29@users.noreply.github.com>
2025-01-21 08:26:50 +0200

    Fix Node.js playground

--- playgrounds/node-env/index.js
+++ playgrounds/node-env/index.js
@@ -1,29 +1,33 @@
-const { instantMeiliSearch } = require('@meilisearch/instant-meilisearch')
-const { MeiliSearch } = require('meilisearch')
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+import { MeiliSearch } from 'meilisearch';
 
+const { searchClient } = instantMeiliSearch(
+  'http://localhost:7700',
+  'masterKey',
+  {},
+);
+const msClient = new MeiliSearch({
+  host: 'http://localhost:7700',
+  apiKey: 'masterKey',
+});
 
-const { searchClient } = instantMeiliSearch('http://localhost:7700', 'masterKey', {})
-const msClient = new MeiliSearch({ host: 'http://localhost:7700', apiKey: 'masterKey'})
+try {
+  const task1 = await msClient.index('node_test').addDocuments([]);
+  await msClient.waitForTask(task1.taskUid);
+  await searchClient.search([
+    {
+      indexName: 'node_test',
+      params: {
+        query: '',
+      },
+    },
+  ]);
 
-;(async() => {
-  try {
-    const task1 = await msClient.index('node_test').addDocuments([])
-    await msClient.waitForTask(task1.taskUid)
-    await searchClient.search([
-      {
-        indexName: 'node_test',
-        params: {
-          query: '',
-        },
-      }
-    ])
-
-    const task2 = await msClient.index('node_test').delete()
-    await msClient.waitForTask(task2.taskUid)
-    process.exit(0)
-  } catch(e) {
-    console.log(e)
-    console.error('Could not run the `umd` build in a node environment')
-    process.exit(1)
-  }
-})()
+  const task2 = await msClient.index('node_test').delete();
+  await msClient.waitForTask(task2.taskUid);
+  process.exit(0);
+} catch (e) {
+  console.log(e);
+  console.error('Could not run the `umd` build in a node environment');
+  process.exit(1);
+}
